25d34ed3b254e923b1fd8d59d469775a71ff8250
Edilson-04 <edilsdondelpiero@gmail.com>
2023-11-17 20:41:14 -0500

    directivas y pipes

--- src/app/ejemplos/directivas-pipes/directivas-pipes.component.ts
+++ src/app/ejemplos/directivas-pipes/directivas-pipes.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./directivas-pipes.component.css']
 })
 export class DirectivasPipesComponent {
+  activo = 0;
+  alumnos = [
+    { nombre: "pepito" },
+    { nombre: "carlos" },
+    { nombre: "jose" },
+    { nombre: "rodrigo" },
+  ];
+  cursos = [
+    { nombre: "DAW2", precio: 8000 },
+    { nombre: "Base de datps", precio: 5000 },
+    { nombre: "Desarrollo de aplicaciones moviles 2", precio: 10000 },
+    { nombre: "angular", precio: 3000 },
+  ];
+
+  fechaActual = new Date();
+
+
+  mostrarAlumnos(): void {
+    this.activo = 0;
+  }
+
+  mostrarCursos(): void {
+    this.activo = 1;
+  }
+
+
+
 
 }
